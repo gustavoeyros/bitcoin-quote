abc952d2f700251a2bf51bbdf73b9d25f28e261a
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-04-12 09:44:39 -0300

    feat: generate candles

--- candle-gen/src/index.ts
+++ candle-gen/src/index.ts
@@ -1,8 +1,10 @@
 import { config } from "dotenv";
 import axios from "axios";
+import Period from "./enums/Period";
+import Candle from "./models/Candle";
 config();
 
-const readMarketPrice = async (): Promise<Number> => {
+const readMarketPrice = async (): Promise<number> => {
   const result = await axios.get(process.env.BITCOIN_API);
   const data = result.data;
   const price = data.bitcoin.usd;
@@ -10,4 +12,22 @@ const readMarketPrice = async (): Promise<Number> => {
   return price;
 };
 
-readMarketPrice();
+const generateCandles = async () => {
+  while (true) {
+    console.log("------------------------");
+    console.log("Generating new candle...");
+    const loopTimes = Period.ONE_MINUTE / Period.TEN_SECONDS;
+    const candle = new Candle("BTC");
+    for (let i = 0; i < loopTimes; i++) {
+      const price = await readMarketPrice();
+      candle.addValue(price);
+      console.log(`Market price #${i + 1} of ${loopTimes}`);
+      await new Promise((t) => setTimeout(t, Period.TEN_SECONDS));
+    }
+    candle.closeCandle();
+    console.log("Candle close!");
+    console.log(candle.toSimpleObject());
+  }
+};
+
+generateCandles();
