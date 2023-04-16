5ee13ad2628b7071396610788eca77bb05442803
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-04-16 12:31:17 -0300

    feat: consume messages method

--- candle-api/src/messages/CandleMessageChannel.ts
+++ candle-api/src/messages/CandleMessageChannel.ts
@@ -3,6 +3,7 @@ import { Server } from "socket.io";
 import * as http from "http";
 import CandleController from "src/controllers/CandleController";
 import { Channel, connect } from "amqplib";
+import { Candle } from "src/models/CandleModel";
 config();
 
 export default class CandleMessageChannel {
@@ -19,9 +20,10 @@ export default class CandleMessageChannel {
       },
     });
     this._io.on("connection", () => console.log("websocket on!"));
+    this._createMessageChannel();
   }
 
-  async createMessageChannel() {
+  private async _createMessageChannel() {
     try {
       const connection = await connect(process.env.AMQP_SERVER);
       this._channel = await connection.createChannel();
@@ -30,5 +32,21 @@ export default class CandleMessageChannel {
       console.log("Connection to RabbitMQ failed!");
       console.log(error);
     }
+  }
+
+  consumeMessages() {
+    this._channel.consume(process.env.QUEUE, async (msg) => {
+      const candleObj = JSON.parse(msg.content.toString());
+      console.log("Message received");
+      console.log(candleObj);
+      this._channel.ack(msg);
+
+      const candle: Candle = candleObj;
+      await this._candleCtrl.save(candle);
+      console.log("Candle saved to db");
+      this._io.emit(process.env.SOCKET_EVENT_NAME, candle);
+      console.log("New candle emited!");
+    });
+    console.log("Consumer started.");
   }
 }
