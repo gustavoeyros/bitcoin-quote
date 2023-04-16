80f50572a1867fce566442c0274a394105a1cc7f
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-04-16 12:56:35 -0300

    feat: candle routes

--- candle-api/src/messages/CandleMessageChannel.ts
+++ candle-api/src/messages/CandleMessageChannel.ts
@@ -1,9 +1,9 @@
 import { config } from "dotenv";
 import { Server } from "socket.io";
 import * as http from "http";
-import CandleController from "src/controllers/CandleController";
+import CandleController from "../controllers/CandleController";
 import { Channel, connect } from "amqplib";
-import { Candle } from "src/models/CandleModel";
+import { Candle } from "../models/CandleModel";
 config();
 
 export default class CandleMessageChannel {
@@ -20,7 +20,6 @@ export default class CandleMessageChannel {
       },
     });
     this._io.on("connection", () => console.log("websocket on!"));
-    this._createMessageChannel();
   }
 
   private async _createMessageChannel() {
@@ -34,19 +33,22 @@ export default class CandleMessageChannel {
     }
   }
 
-  consumeMessages() {
-    this._channel.consume(process.env.QUEUE, async (msg) => {
-      const candleObj = JSON.parse(msg.content.toString());
-      console.log("Message received");
-      console.log(candleObj);
-      this._channel.ack(msg);
+  async consumeMessages() {
+    await this._createMessageChannel();
+    if (this._channel) {
+      this._channel.consume(process.env.QUEUE, async (msg) => {
+        const candleObj = JSON.parse(msg.content.toString());
+        console.log("Message received");
+        console.log(candleObj);
+        this._channel.ack(msg);
 
-      const candle: Candle = candleObj;
-      await this._candleCtrl.save(candle);
-      console.log("Candle saved to db");
-      this._io.emit(process.env.SOCKET_EVENT_NAME, candle);
-      console.log("New candle emited!");
-    });
-    console.log("Consumer started.");
+        const candle: Candle = candleObj;
+        await this._candleCtrl.save(candle);
+        console.log("Candle saved to db");
+        this._io.emit(process.env.SOCKET_EVENT_NAME, candle);
+        console.log("New candle emited!");
+      });
+      console.log("Consumer started.");
+    }
   }
 }
--- candle-api/src/server.ts
+++ candle-api/src/server.ts
@@ -2,13 +2,17 @@ import { config } from "dotenv";
 import { app } from "./app";
 import { connectToMongoDB } from "./config/db";
 import { connection } from "mongoose";
+import CandleMessageChannel from "./messages/CandleMessageChannel";
 
 const createServer = async () => {
   config();
-
-  await connectToMongoDB();
   const PORT = process.env.PORT;
   const server = app.listen(PORT, () => console.log("App running!"));
+  await connectToMongoDB();
+
+  const candleMsgChannel = new CandleMessageChannel(server);
+  candleMsgChannel.consumeMessages();
+
   process.on("SIGINT", async () => {
     await connection.close();
     server.close();
